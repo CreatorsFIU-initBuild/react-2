a014f94f63901a7f3709eecadf69a4b819271229
Austinh1512 <Austinhouchens1510@gmail.com>
2025-04-04 15:21:20 -0400

    added options for sidebar

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,63 +1,70 @@
-import { Link, NavLink } from "react-router-dom";
-import { FaRegListAlt, FaShoppingCart, FaSignOutAlt } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import {
+  FaHeart,
+  FaRegUserCircle,
+  FaShoppingCart,
+  FaSignOutAlt,
+} from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div style={{ padding: "1.5rem" }}>
-      <div style={{ marginBottom: "1.5rem" }}>
-        <Link
-          to="/dashboard"
-          className="block text-white text-2xl"
-          style={{ textDecoration: "none" }}
-        >
-          CreatorsFIU
-        </Link>
+    <div className="!p-4 flex flex-col h-full">
+      <div className="!mb-6">
+        <h2 className="font-medium text-center !my-6 text-3xl">John Doe</h2>
       </div>
-      <h2
-        className="font-medium text-center"
-        style={{ marginBottom: "1.5rem", fontSize: "2rem" }}
-      >
-        Dashboard
-      </h2>
 
       <nav className="flex flex-col">
-        <NavLink
-          to="/dashboard/seller"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-gray-700 text-white rounded flex items-center"
-              : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
-          }
-          style={{
-            padding: "0.75rem 1rem",
-            textDecoration: "none",
-            color: "white",
-            margin: "1rem 0",
-          }}
-        >
-          <FaRegListAlt style={{ marginRight: "0.5rem" }} />
-          My Listings
-        </NavLink>
-        <NavLink
-          to="/dashboard/buyer"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-gray-700 text-white rounded flex items-center"
-              : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
-          }
-          style={{
-            padding: "0.75rem 1rem",
-            textDecoration: "none",
-            color: "white",
-          }}
-        >
-          <FaShoppingCart style={{ marginRight: "0.5rem" }} />
-          <span>My Purchases</span>
-        </NavLink>
+        <div>
+          <p>Buyer</p>
+          <NavLink
+            to="/dashboard/favorites"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white rounded flex items-center"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
+            }
+            style={{
+              padding: "0.75rem 1rem",
+              textDecoration: "none",
+              color: "white",
+              margin: "1rem 0",
+            }}
+          >
+            <FaHeart style={{ marginRight: "0.5rem" }} />
+            Favorites
+          </NavLink>
+          <NavLink
+            to="/dashboard/cart"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white rounded flex items-center"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
+            }
+            style={{
+              padding: "0.75rem 1rem",
+              textDecoration: "none",
+              color: "white",
+              margin: "1rem 0",
+            }}
+          >
+            <FaShoppingCart style={{ marginRight: "0.5rem" }} />
+            Cart
+          </NavLink>
+        </div>
       </nav>
-      <div style={{ marginTop: "1.5rem" }}>
+
+      <div className="flex flex-col gap-5 !mt-auto">
+        <NavLink
+          className="w-full bg-gray-400 hover:bg-gray-500 text-white rounded flex items-center justify-center cursor-pointer"
+          style={{ padding: "0.5rem 1rem" }}
+          to="/dashboard/profile"
+        >
+          <FaRegUserCircle style={{ marginRight: "0.5rem" }} />
+          <span>Manage Profile</span>
+        </NavLink>
+
         <button
-          className="w-full bg-red-500 hover:bg-red-600 text-white rounded flex items-center justify-center"
+          className="w-full bg-red-500 hover:bg-red-600 text-white rounded flex items-center justify-center cursor-pointer"
           style={{ padding: "0.5rem 1rem" }}
         >
           <FaSignOutAlt style={{ marginRight: "0.5rem" }} />
